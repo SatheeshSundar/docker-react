afebad891c5e1cfd981b0dbe410284df77cc937d
root <root@ip-172-31-36-218.us-east-2.compute.internal>
2019-10-31 23:45:56 +0000

    added travis file

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        How are you doing! 
+        How are you doing 
         </p>
         <a
           className="App-link"
